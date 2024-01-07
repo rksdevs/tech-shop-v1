65fe22339b6767a07b68b2475a366a6bf058999a
Rakesh <mailto.dev.rakesh@gmail.com>
2024-01-07 19:27:19 +0530

    bootstrapped, added header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Container } from 'react-bootstrap';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 const App = () => {
   return (
-    <h1>Welcome to Tech Shop</h1>
+    <>
+      <Header />
+        <main className='py-3'>
+          <Container>
+            <h1>Welcome to Tech Shop</h1>
+          </Container>
+        </main>
+        <Footer />
+    </>
   )
 }
 
