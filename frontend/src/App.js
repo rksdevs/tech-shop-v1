a26e8c8eee3d98ae2c6f8ad8a84efe06373b188a
Rakesh <mailto.dev.rakesh@gmail.com>
2024-01-07 20:15:35 +0530

    Product list sample added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Container } from 'react-bootstrap';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import HomeScreen from './Screens/HomeScreen';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <main className='py-3'>
           <Container>
             <h1>Welcome to Tech Shop</h1>
+            <HomeScreen />
           </Container>
         </main>
         <Footer />
