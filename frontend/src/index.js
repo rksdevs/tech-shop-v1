700fa6bb996060f9ffb7006fd7e5c800f2a02dc3
Rakesh <mailto.dev.rakesh@gmail.com>
2024-01-07 23:29:03 +0530

    express server, and fetching data from backend folder - completed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,11 +6,13 @@ import './assets/styles/bootstrap.custom.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import HomeScreen from './Screens/HomeScreen';
+import ProductScreen from './Screens/ProductScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
       <Route index={true} path='/' element={<HomeScreen />} />
+      <Route path='/product/:id' element={<ProductScreen />} />
     </Route>
   )
 )
