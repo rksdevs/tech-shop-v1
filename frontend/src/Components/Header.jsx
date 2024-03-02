b8ceec993119851a692fafd4d50fb156fb923c94
Rakesh <mailto.dev.rakesh@gmail.com>
2024-03-02 13:58:48 +0530

    Search box functionality added

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -7,6 +7,7 @@ import { useNavigate } from "react-router-dom";
 import { useLogoutMutation } from "../slices/usersApiSlice";
 import { logout } from "../slices/authSlice";
 import logo from "../assets/logo.png";
+import SearchBox from "./SearchBox";
 
 const Header = () => {
   const { cartItems } = useSelector((state) => state.cart);
@@ -40,6 +41,7 @@ const Header = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
+              <SearchBox />
               <LinkContainer to="/buildmypc">
                 <Nav.Link>Build Your PC</Nav.Link>
               </LinkContainer>
--- frontend/src/Screens/HomeScreen.jsx
+++ frontend/src/Screens/HomeScreen.jsx
@@ -4,15 +4,23 @@ import Product from "../Components/Product";
 import { useGetProductsQuery } from "../slices/productApiSlice";
 import Loader from "../Components/Loader";
 import Message from "../Components/Message";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Paginate from "../Components/Paginate";
 
 const HomeScreen = () => {
-  const { pageNumber } = useParams();
-  const { data, isLoading, error } = useGetProductsQuery({ pageNumber });
+  const { keyword, pageNumber } = useParams();
+  const { data, isLoading, error } = useGetProductsQuery({
+    keyword,
+    pageNumber,
+  });
 
   return (
     <>
+      {keyword && (
+        <Link to="/" className="btn btn-light mb-4">
+          Go Back
+        </Link>
+      )}
       {isLoading ? (
         <Loader />
       ) : error ? (
@@ -23,13 +31,18 @@ const HomeScreen = () => {
         <>
           <h1>Latest Products</h1>
           <Row>
+            {!data.products.length > 0 && <Message>No Products Found</Message>}
             {data.products.map((product) => (
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                 <Product product={product} />
               </Col>
             ))}
           </Row>
-          <Paginate page={data.page} pages={data.pages} />
+          <Paginate
+            page={data.page}
+            pages={data.pages}
+            keyword={keyword ? keyword : ""}
+          />
         </>
       )}
     </>
